893c842f9948a6f133e260d0ef1dd1f7517e36dd
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-06 23:55:54 -0300

    Create Pagination Component

---
+++ src/app/pagination/pagination.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+
+@Component({
+  selector: 'cm-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss']
+})
+export class PaginationComponent implements OnInit {
+
+  @Output() childEvent = new EventEmitter();
+  @Input() nextPage: any
+  @Input() previousPage: any
+  @Input() currentPage: any
+
+  ngOnInit(): void {
+  }
+
+  updateListAndPagination( page ){
+    this.childEvent.emit( page );
+  }
+}
