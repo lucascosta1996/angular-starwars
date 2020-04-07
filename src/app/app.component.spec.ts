2e049272dcf755cf144a093d8045a9bea5beda43
Lucas Alves Costa <lucas.alves.costa96@gmail.com>
2020-04-07 06:56:49 -0300

    Add Unit Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +22,21 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'dragon'`, () => {
+  it(`should have as title 'nextPage' equal to 2`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('dragon');
+    expect(app.nextPage).toEqual(2);
   });
 
-  it('should render title', () => {
+  it('current page should be 1', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dragon app is running!');
+    const app = fixture.componentInstance;
+    expect(app.currentPage).toBe( 1 );
+  });
+
+  it('next page should be 2', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.nextPage).toBe( 2 );
   });
 });
--- src/app/pagination/pagination.component.spec.ts
+++ src/app/pagination/pagination.component.spec.ts
@@ -19,7 +19,34 @@ describe('PaginationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create Pagination', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('previous button should not exist on the first page', () => {
+    component.currentPage = 1;
+    component.updateListAndPagination( 1 );
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('#previous')).toBeFalsy();
+  });
+  it('next button should not exist on the last page', () => {
+    component.currentPage = 9;
+    component.updateListAndPagination( 9 );
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('#next')).toBeFalsy();
+  });
+
+  it('current page button should be 1', () => {
+    component.currentPage = 1;
+    component.updateListAndPagination( 1 );
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('#current').innerText).toEqual( '1' );
+  });
+
+  it('next page button should be 2', () => {
+    component.nextPage = 2;
+    component.updateListAndPagination( 1 );
+    fixture.detectChanges();
+    expect(fixture.nativeElement.querySelector('#next').innerText).toEqual( '2' );
   });
 });
